6d3c2936106c27459e970d8a58e25ad3b83d78ca
Admin <admin@MacBook-Pro-Admin.local>
2018-07-03 23:11:55 +0300

    First commit

--- InstaPost/AppDelegate.swift
+++ InstaPost/AppDelegate.swift
@@ -19,28 +19,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+//    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+//        let absolute = url.absoluteString
+//        let code = absolute.suffix(from: absolute.range(of: "code=")!.upperBound)
+//        
+//        guard let url = URL(string: "https://api.instagram.com/oauth/access_token?grant_type=authorization_code&client_id=8600a9c359234814baf67e4d4efab5aa&client_secret=8205a206187b4d918d00c205385493a6&code=\(code)") else {
+//            print("URL is equal to nil")
+//            return false
+//        }
+//        var request = URLRequest(url: url)
+//        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+//        request.httpMethod = "POST"
+//        let postString = "code=\(code))"
+//        request.httpBody = postString.data(using: .utf8)
+//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//            guard let data = data, error == nil else {
+//                print("error=\(String(describing: error))")
+//                return
+//            }
+//            
+//            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+//                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+//                print("response = \(String(describing: response))")
+//            }
+//            
+//            //            let responseString = String(data: data, encoding: .utf8)
+//            //            print("responseString = \(String(describing: responseString))")
+//            do {
+//                guard let json = try? JSONSerialization.jsonObject(with: data, options: []) as! [String: Any] else {
+//                    print("problem in get token")
+//                    return
+//                }
+//                let token = json["access_token"] as! String
+//                UserDefaults.standard.set(token, forKey: "token")
+////                DispatchQueue.main.async {
+////                    NotificationCenter.default.post(name: .didFinishAuthorization, object: nil)
+////                }
+//            }
+//        }
+//        task.resume()
+//        return true
+//    }
+    
 }
 
